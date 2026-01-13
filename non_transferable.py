448bb2db3889b72cbaca8d00b5684003918f72b5
DamhanM <davicomull@gmail.com>
2026-01-13 13:38:48 +0000

    Create non_transferable.py
    Python code to examine the proportion of non-transferable votes nationally non-cumulatively, and plots it as a bar chart.
    
    Takes the proportion as the sum of the non-transferable votes over the sum of the valid votes, per count.

---
+++ non_transferable.py
@@ -0,0 +1,35 @@
+def all_counts_bar_chart(ct_lst, vals_lst):
+    
+    plt.figure(figsize=(10, 5))
+    
+    bars = plt.bar(ct_lst, vals_lst, color='skyblue',width=0.4,align = "center")
+    
+    plt.title(f'Non-transferable Votes As A Proportion of Valid Poll Per Count')
+    plt.xlabel('Count')
+    plt.ylabel('Proportion')
+    
+    for bar in bars:
+        yval = bar.get_height()
+        plt.text(bar.get_x(), yval + .005, yval)
+    
+    #save_results_to = "C:/Users/22358313/Desktop/University/4th_Year/FYP/figures/non_transfer-bar_charts-labels/"
+    #file_name = "national-non-t-per-count"
+    #plt.savefig(save_results_to + file_name)
+    plt.show()
+  
+def non_cumul_propn_per_count(df):
+    propn_lst = []
+    for count in range(1,16):
+        
+        count_df = df[df["Count Number"]==count]
+        total_valid_per_count = sum(list(count_df["Valid Poll"].unique()))
+        total_non_t_per_count = -(np.sum(list(count_df["Transfers"].values)))
+        propn = total_non_t_per_count/total_valid_per_count
+
+        propn_lst.append(round(propn,3))
+        
+    all_counts_bar_chart(list(range(1,16)),propn_lst)
+
+#--IMPLEMENTATION
+full_df = pd.read_csv("General_Election_2020_full.csv")
+non_cumul_propn_per_count(full_df)
